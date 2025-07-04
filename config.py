67cc023ce58891fe13ea7b95b19cf17e54473abb
vahid <vahid91630@gmail.com>
2025-07-05 01:50:54 +0330

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+# توکن بات تلگرام
+BOT_TOKEN = os.getenv("BOT_TOKEN") or "توکن‌ات‌رو‌اینجا‌قرار‌بده"
+
+# مسیر فایل‌های داده
+CUSTOMER_DB = "data/customers.json"
+SALES_DB = "data/sales.json"
+INVENTORY_DB = "data/inventory.json"
