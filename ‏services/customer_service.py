08cd3c6cc5e10421734431fc24d23027dab0aa56
vahid <vahid91630@gmail.com>
2025-07-05 02:13:40 +0330

    Update customer_service.py

--- ‏services/customer_service.py
+++ ‏services/customer_service.py
@@ -1,1 +1,9 @@
-
+def add_customer(data, db_path):
+    import json, os
+    customers = []
+    if os.path.exists(db_path):
+        with open(db_path, "r", encoding="utf-8") as f:
+            customers = json.load(f)
+    customers.append(data)
+    with open(db_path, "w", encoding="utf-8") as f:
+        json.dump(customers, f, ensure_ascii=False, indent=2)
